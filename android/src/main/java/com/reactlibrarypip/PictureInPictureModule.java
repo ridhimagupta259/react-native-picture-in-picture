8e4e2db3a6c7531b3bfadb3608bc8b44e4ae4fb0
ridhimagupta259 <60702108+ridhimagupta259@users.noreply.github.com>
2021-05-21 11:53:01 +0530

    added home button functionality

--- android/src/main/java/com/reactlibrarypip/PictureInPictureModule.java
+++ android/src/main/java/com/reactlibrarypip/PictureInPictureModule.java
@@ -10,13 +10,37 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-public class PictureInPictureModule extends ReactContextBaseJavaModule {
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.LifecycleEventListener;
+import android.app.PictureInPictureParams;
+import android.os.Build;
+import android.util.Rational;
+
+
+
+public class PictureInPictureModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
 
     private final ReactApplicationContext reactContext;
+
+    private static final int ASPECT_WIDTH = 3;
+    private static final int ASPECT_HEIGHT = 4;
+    private boolean isPipSupported = false;
+    private boolean isCustomAspectRatioSupported = false;
+    private boolean isPipListenerEnabled = false;
+    private Rational aspectRatio;
+
+
 
     public PictureInPictureModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+
+
+        reactContext.addLifecycleEventListener(this);
+        isPipSupported = true;
+        isCustomAspectRatioSupported = true;
+        aspectRatio = new Rational(ASPECT_WIDTH, ASPECT_HEIGHT);
     }
 
     @Override
@@ -32,15 +56,23 @@ public class PictureInPictureModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void start() {
-        final Activity activity = getCurrentActivity();
-        if(hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)){
-            activity.enterPictureInPictureMode();
-            return;
-        } else {
-            Context context = getReactApplicationContext();
-            String text = "Your device is not supported";
-            Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+         if (isPipSupported) {
+            if (isCustomAspectRatioSupported) {
+                PictureInPictureParams params = new PictureInPictureParams.Builder()
+                        .setAspectRatio(this.aspectRatio).build();
+                getCurrentActivity().enterPictureInPictureMode(params);
+            } else
+                getCurrentActivity().enterPictureInPictureMode();
         }
+        // final Activity activity = getCurrentActivity();
+        // if(hasSys temFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)){
+        //     activity.enterPictureInPictureMode();
+        //     return;
+        // } else {
+        //     Context context = getReactApplicationContext();
+        //     String text = "Your device is not supported";
+        //     Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+        // }
 
     }
 
@@ -48,4 +80,37 @@ public class PictureInPictureModule extends ReactContextBaseJavaModule {
 //        FeatureInfo.getSystemAvailableFeatures();
         return true;
     }
+
+
+    @ReactMethod
+    public void configureAspectRatio(Integer width, Integer height) {
+        aspectRatio = new Rational(width, height);
+    }
+
+    @ReactMethod
+    public void enableAutoPipSwitch() {
+        isPipListenerEnabled = true;
+    }
+
+    @ReactMethod
+    public void disableAutoPipSwitch() {
+        isPipListenerEnabled = false;
+    }
+
+    @Override
+    public void onHostResume() {
+    }
+
+    @Override
+    public void onHostPause() {
+        if (isPipSupported && isPipListenerEnabled) {
+            start();
+        }
+    }
+
+    @Override
+    public void onHostDestroy() {
+    }
+
 }
+
